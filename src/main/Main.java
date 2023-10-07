254c86c4c8964777e5945c37f46e26cd58a51165
HP <HP@DESKTOP-TK2OJN6>
2023-10-07 17:02:39 +0700

    commit 14

---
+++ src/main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import controller.ManageUser;
+
+/**
+ *
+ * @author HP
+ */
+public class Main {
+    public static void main(String[] args) {
+        String title= "USER MANAGEMENT SYSTEM";
+        String[] s = new String[] {"Create a new account", "Login system", "Exit"};
+        new ManageUser(title, s).run();
+    }
+}
